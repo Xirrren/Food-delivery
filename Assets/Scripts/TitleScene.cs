c723251620b55ec12626d81db4ecbd0f1016faaf
rStar <starlove1247@gmail.com>
2025-09-14 13:07:25 +0800

    建立標題場景與進入遊戲按鈕

---
+++ Assets/Scripts/TitleScene.cs
@@ -0,0 +1,24 @@
+using System;
+using Microsoft.Unity.VisualStudio.Editor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace DefaultNamespace
+{
+    public class TitleScene : MonoBehaviour
+    {
+        [SerializeField]
+        private Button enterGameButton;
+
+        private void Start()
+        {
+         enterGameButton.onClick.AddListener(EnterGame);   
+        }
+
+        private void EnterGame()
+        {
+            SceneManager.LoadScene("GameScene");
+        }
+    }
+}
